119c2c77227f48b3a4b7afad7b82f49dfe8437cf
houssam-wees <houssamwees89@gmail.com>
2024-07-11 13:43:43 +0200

    last changes

--- app.py
+++ app.py
@@ -4,34 +4,39 @@ from typing import Dict
 import pandas as pd
 from dateutil import parser
 from datetime import date
+import uvicorn
 
-app = FastAPI()
 
-# Load and process the data
+# Loading and processing csv data
 elev_data = pd.read_csv('./Fahrtdaten.csv')
 elev_data['ts'] = pd.to_datetime(elev_data['ts'], unit='ms')
-
 elev_data['date'] = elev_data['ts'].dt.date
 
 
+app = FastAPI()
+
 class RidesPerDayResponse(BaseModel):
     rides_per_day: Dict[str, int]
-    
-@app.get("/hello")
+
+# this only to have root on the api and through any errors   
+@app.get("/")
 def read_root():
     return {"message": "Hello, World!"}
 
+
 @app.get("/rides_per_day", response_model=RidesPerDayResponse)
-def get_rides_per_day(date_start: str = Query(...), date_end: str = Query(...)):
+def get_rides_per_day(date_start: str = Query(...), date_end: str = Query(...)) -> dict:
     try:
+        # here I am using the parser to get starting and ending dates
         start_date = parser.parse(date_start).date()
         end_date = parser.parse(date_end).date()
     except ValueError:
         raise HTTPException(status_code=400, detail="Invalid date format")
     try:
-
+        # here I am filtering the query
         elev_data_range = elev_data[(elev_data['date'] >= start_date) & (elev_data['date'] <= end_date)]
 
+        # here I groupped the data by data and took the size of that 
         rides_per_day = elev_data_range.groupby('date').size()
         rides_per_day_dict = {str(date_index):count for date_index, count in rides_per_day.items()}
 
@@ -43,6 +48,6 @@ def get_rides_per_day(date_start: str = Query(...), date_end: str = Query(...)):
     return {"rides_per_day": rides_per_day_dict}
     
 
-#if __name__ == '__main__':
- #   import uvicorn
+#f __name__ == '__main__':
+   
  #   uvicorn.run(app, host="127.0.0.1", port=12345)
--- test_elev_app.py
+++ test_elev_app.py
@@ -1,5 +1,6 @@
 import unittest
 from fastapi.testclient import TestClient
+
 from app import app
 
 client = TestClient(app)
